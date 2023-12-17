a335b8a9a84190863fb42a22798b08e5679e6405
Tayeb <40003626@parisnanterrefr>
2023-12-17 18:46:26 +0100

    Changement calendrier, ajout firestore

--- app/(auth)/calendar.tsx
+++ app/(auth)/calendar.tsx
@@ -1,11 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import { SafeAreaView, StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, StyleSheet, Text, View, Modal, TextInput, Button } from 'react-native';
 import { Agenda } from 'react-native-calendars';
-// @ts-ignore
 import ical from 'ical.js';
 import * as SplashScreen from 'expo-splash-screen';
+import { collection, doc, setDoc, getDoc } from 'firebase/firestore';
+import {FIRESTORE_DB} from "../../FirebaseConfig";
+import { useUser } from '@clerk/clerk-expo';
 
-// Mise à jour du type Item pour inclure startTime et endTime
+const db = FIRESTORE_DB;
+
 type Item = {
     name: string;
     location: string;
@@ -19,10 +22,46 @@ const Calendar: React.FC = () => {
     const [items, setItems] = useState<{ [key: string]: Item[] }>({});
     const [url, setUrl] = useState<string>('');
     const [loading, setLoading] = useState<boolean>(false);
+    const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+    const { user } = useUser();
+    const numE = user?.id;
 
     useEffect(() => {
         SplashScreen.preventAutoHideAsync();
+        loadUrlFromFirestore();
     }, []);
+
+    useEffect(() => {
+        if (url) {
+            getData();
+        }
+    }, [url]);
+
+    const loadUrlFromFirestore = async () => {
+        const documentId = `ical${numE}`; // numE changes according to your logic
+        const urlRef = doc(db, 'icalUsers', documentId);
+        try {
+            const docSnap = await getDoc(urlRef);
+            if (docSnap.exists()) {
+                const data = docSnap.data();
+                setUrl(data.url);
+            } else {
+                console.log('No such document in Firestore!');
+            }
+        } catch (error) {
+            console.error('Error fetching URL from Firestore:', error);
+        }
+    };
+
+    const saveUrlToFirestore = async (url: string) => {
+        const documentId = `ical${numE}`; // numE changes according to your logic
+        const urlRef = doc(db, 'icalUsers', documentId);
+        try {
+            await setDoc(urlRef, { url: url });
+        } catch (error) {
+            console.error('Error saving URL to Firestore:', error);
+        }
+    };
 
     const getData = async () => {
         if (!url) return;
@@ -32,11 +71,9 @@ const Calendar: React.FC = () => {
 
         try {
             const response = await fetch(url);
-
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
-
             const responseText = await response.text();
             const jcalData = ical.parse(responseText);
             const comp = new ical.Component(jcalData);
@@ -49,11 +86,8 @@ const Calendar: React.FC = () => {
 
                 const startDate = vevent.startDate.toJSDate();
                 const endDate = vevent.endDate.toJSDate();
-
-                // Formatage pour obtenir seulement l'heure au format souhaité
-                const startTime = startDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }).replace(':', 'h');
-                const endTime = endDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }).replace(':', 'h');
-
+                const startTime = startDate.toISOString().split('T')[1].substring(0, 5);
+                const endTime = endDate.toISOString().split('T')[1].substring(0, 5);
                 const dateString = startDate.toISOString().split('T')[0];
 
                 const item: Item = {
@@ -72,18 +106,18 @@ const Calendar: React.FC = () => {
                 mappedItems[dateString].push(item);
             });
 
-            setTimeout(() => {
-                setItems(mappedItems);
-                setUrl('');
-            }, 500);
+            for (let date in mappedItems) {
+                mappedItems[date].sort((a, b) => a.startTime.localeCompare(b.startTime));
+            }
+
+            setItems(mappedItems);
         } catch (error) {
             console.error('Error fetching or parsing data:', error);
         } finally {
             setLoading(false);
-            await SplashScreen.hideAsync();
+            SplashScreen.hideAsync();
         }
     };
-
 
     const renderItem = (item: Item) => {
         return (
@@ -91,22 +125,36 @@ const Calendar: React.FC = () => {
                 <Text style={styles.itemTitle}>{item.name}</Text>
                 <Text style={styles.itemLocation}>{item.location}</Text>
                 <Text style={styles.itemDescription}>{item.description}</Text>
-                <Text style={styles.itemHour}> {item.startTime} - {item.endTime}</Text>
+                <Text style={styles.itemHour}>{item.startTime} - {item.endTime}</Text>
             </View>
         );
     };
 
+    const handleSetUrl = () => {
+        setIsModalVisible(false);
+        saveUrlToFirestore(url);
+        getData();
+    };
+
     return (
         <SafeAreaView style={styles.safe}>
-            <View style={styles.inputContainer}>
-                <TextInput
-                    style={styles.input}
-                    onChangeText={setUrl}
-                    value={url}
-                    placeholder="Enter Calendar URL"
-                />
-                <Button title="Load Calendar" onPress={getData} disabled={loading} />
-            </View>
+            <Button title="Settings" onPress={() => setIsModalVisible(true)} />
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={isModalVisible}
+                onRequestClose={() => setIsModalVisible(false)}
+            >
+                <View style={styles.modalView}>
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={setUrl}
+                        value={url}
+                        placeholder="Enter Calendar URL"
+                    />
+                    <Button title="Set Calendar URL" onPress={handleSetUrl} />
+                </View>
+            </Modal>
             <Agenda items={items} renderItem={renderItem} />
         </SafeAreaView>
     );
@@ -116,16 +164,27 @@ const styles = StyleSheet.create({
     safe: {
         flex: 1,
     },
-    inputContainer: {
-        padding: 10,
-        backgroundColor: '#fff',
+    modalView: {
+        marginTop: 50,
+        marginHorizontal: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
     },
     input: {
         height: 40,
-        margin: 12,
+        width: '100%',
+        marginVertical: 12,
         borderWidth: 1,
-        borderColor: '#ddd',
-        borderRadius: 4,
         padding: 10,
     },
     itemContainer: {
@@ -142,24 +201,19 @@ const styles = StyleSheet.create({
     itemTitle: {
         fontSize: 16,
         fontWeight: 'bold',
-        lineHeight: 20, // Hauteur de ligne ajustée pour le titre
     },
     itemLocation: {
         fontSize: 14,
         color: '#555',
-        lineHeight: 18, // Hauteur de ligne ajustée pour l'emplacement
     },
     itemDescription: {
         fontSize: 14,
         color: '#666',
-        lineHeight: 18, // Hauteur de ligne ajustée pour la description
     },
     itemHour: {
         fontSize: 14,
         color: '#666',
-        lineHeight: 18, // Hauteur de ligne ajustée pour l'heure
     },
 });
-
 
 export default Calendar;
